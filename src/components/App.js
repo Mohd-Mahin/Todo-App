3ad275f36d31f87c7bd5f20238d48bc713579481
Mohd-Mahin <mahinit3016@gmail.com>
2018-10-15 14:02:29 +0530

    final changes

--- src/components/App.js
+++ src/components/App.js
@@ -11,13 +11,10 @@ export default class App extends Component {
         this.state = {
            todoHeading: '',
            todoTextArea: '',
-           todoList: []
+           todoList: [],
         }
-    }
-
-    componentWillMount() {
-        $('details:first-of-type').attr('open');
-    }
+        this.editObj = null
+    }   
 
     handleTodoHeading(e) {
         let todoHeading = e.target.value;
@@ -32,30 +29,51 @@ export default class App extends Component {
             todoTextArea
         });
     }
+    
+    clearTextData () {
+        this.setState({
+            todoHeading: '',
+            todoTextArea: ''
+        });
+    }
 
     handleSubmit(evt) {
         evt.preventDefault();
-        if (!this.state.todoHeading || !this.state.todoTextArea) return alert("Fields cannot be empty!!!");
-        this.setState((prevState) => {
-            return {
-                todoList: prevState.todoList.concat({
-                    id: prevState.todoList.length + 1,
-                    heading: prevState.todoHeading,
-                    textArea: prevState.todoTextArea,
-                    isCompleted: false
-                })
-            }   
-        }, () => {
-            this.setState({
-                todoHeading: '',
-                todoTextArea: ''
-            });
-        })
-        
+        if (this.setTextIsEdit()) {
+            this.setState(prevState => {
+                return {
+                    todoList: prevState.todoList.map(items => {
+                        if (items.id == this.editObj.id) {
+                            return {
+                                id: this.editObj.id,
+                                heading: prevState.todoHeading,
+                                textArea: prevState.todoTextArea,
+                                isCompleted: false
+                            }
+                        }
+                        return items;
+                    })
+                }
+            }, this.clearTextData.bind(this))
+        } else {
+            if (!this.state.todoHeading || !this.state.todoTextArea) return alert("Fields cannot be empty!!!");
+            this.setState((prevState) => {
+                return {
+                    todoList: prevState.todoList.concat({
+                        id: prevState.todoList.length + 1,
+                        heading: prevState.todoHeading,
+                        textArea: prevState.todoTextArea,
+                        isCompleted: false
+                    })
+                }   
+            },this.clearTextData.bind(this))
+        }
     }
 
-    handleIsComplete() {
-        alert('changed')
+    handleIsComplete(evt) {
+        // checkbox check
+        //console.log(evt);
+
     }
 
     leftContainer() {
@@ -71,9 +89,36 @@ export default class App extends Component {
                         onChange={this.handleTodoTextArea.bind(this)}
                         value={this.state.todoTextArea}
                     />
-                    <div style={{ width: '100%', height: 'auto', marginTop: 30,marginBottom: 30, justifyContent: 'center', display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
-                        <button type="submit" style={{width: 100, height: 21}}> Add </button>
-                        <input type="checkbox" style={{ margin: 10, marginLeft: 20}} id="isCompleted" onChange={this.handleIsComplete.bind(this)}/>
+                    <div 
+                        style={{ 
+                            width: '100%', 
+                            height: 'auto', 
+                            marginTop: 30,
+                            marginBottom: 30, 
+                            justifyContent: 'center', 
+                            display: 'flex', 
+                            flexDirection: 'row', 
+                            alignItems: 'center'
+                        }}
+                    >
+                        <button 
+                            type="submit" 
+                            style={{width: 100, height: 21}}
+                        > 
+                            {
+                                this.setTextIsEdit()
+                                ?
+                                'Edit'
+                                :
+                                'Add'
+                            } 
+                        </button>
+                        <input 
+                            type="checkbox" 
+                            style={{ margin: 10, marginLeft: 20}} 
+                            id="isCompleted" 
+                            onChange={this.handleIsComplete.bind(this)}
+                        />
                         <label htmlFor="isCompleted">Completed</label>
                     </div>
                 </form>
@@ -81,9 +126,29 @@ export default class App extends Component {
         )
     }
 
+    setTextIsEdit() {
+        let _this = this;
+        let text = (this.editObj && this.state.todoList.find(e => e.id == _this.editObj.id));
+        if (text) return (text.isCompleted)
+        return false
+    }
+
     handleEdit(index) {
         let obj = this.state.todoList.find(item => {
-            return item.id == index;
+             return item.id == index;
+        });
+        this.editObj = obj;
+        this.setState((prevState) => {
+            return {
+                todoHeading: obj.heading,
+                todoTextArea: obj.textArea,
+                todoList: prevState.todoList.map(item => {
+                    if (item.id == obj.id) {
+                        item.isCompleted = true
+                    }
+                    return item;
+                })
+            }
         });
     }
 
@@ -103,15 +168,32 @@ export default class App extends Component {
     }
 
     rightContainer() {
-        return this.state.todoList.map((todoItem, index )=> {
+        return this.state.todoList && this.state.todoList.map((todoItem, index )=> {
             return (
                 <details key={index}>
                     <summary style={{color: 'maroon', outline: 'none'}}>
-                        <span style={{ display: 'inline-block', width: '5%', verticalAlign: 'top' }}>{todoItem.id})</span>
-                        <span style={{ display: 'inline-block', width: '65%', verticalAlign: 'top' }}>{todoItem.heading}</span>
+                        <span style={{ display: 'inline-block', width: '5%', verticalAlign: 'top' }}>
+                            {todoItem.id})
+                        </span>
+                        <span style={{ display: 'inline-block', width: '65%', verticalAlign: 'top' }}>
+                            {todoItem.heading}
+                        </span>
                         <span style={{ display: 'inline-block', width: '25%' }}>
-                            <button type="button" style={{width: '100%', height: 21, marginRight: 5}} onClick={this.handleEdit.bind(this, todoItem.id)}>Edit</button>
-                            <button type="button" style={{width: '100%', height: 21, marginTop: 5}} onClick={this.handleDelete.bind(this, todoItem.id)}>Delete</button>
+                            <button 
+                                type="button" 
+                                style={{width: '100%', height: 21, marginRight: 5, opacity: this.setTextIsEdit() ? 0.3 : 1}} 
+                                onClick={this.handleEdit.bind(this, todoItem.id)}
+                                disabled={this.setTextIsEdit()}
+                            >
+                                Edit
+                            </button>
+                            <button 
+                                type="button" 
+                                style={{width: '100%', height: 21, marginTop: 5}} 
+                                onClick={this.handleDelete.bind(this, todoItem.id)}
+                            >
+                                Delete
+                            </button>
                         </span>
                     </summary>
                     <p className="todo-content">{todoItem.textArea}</p>
